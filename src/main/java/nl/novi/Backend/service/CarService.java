21cc839cddc172ea11e35d04e30fff8336d7009a
qixiaoling <x.qi@novi-education.nl>
2021-01-12 15:24:10 +0100

    add CarService

--- src/main/java/nl/novi/Backend/service/CarService.java
+++ src/main/java/nl/novi/Backend/service/CarService.java
@@ -4,10 +4,13 @@ import nl.novi.Backend.model.Car;
 import nl.novi.Backend.model.Customer;
 import nl.novi.Backend.repo.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class CarService {
 
@@ -29,6 +32,31 @@ public class CarService {
         List<Car> cars = new ArrayList<>();
         carRepository.save(car);
         return cars;
+    }
+    public Car getCarById(String numberPlate ){
+        Optional<Car> possibleCar = carRepository.findById(numberPlate);
+        if(possibleCar.isPresent()){
+            return possibleCar.get();
+        }
+        return null;
+    }
+
+    public Car updateCarById(String numberPlate, Car car){
+        Optional<Car> possibleCar = carRepository.findById(numberPlate);
+        if(possibleCar.isPresent()){
+            Car aCar = possibleCar.get();
+            return aCar;
+        }
+        return null;
+    }
+
+    public ResponseEntity<?> deleteCustomerById(String numberPlate){
+        Optional<Car> possibleCar = carRepository.findById(numberPlate);
+        if(possibleCar.isPresent()){
+            carRepository.deleteById(numberPlate);
+            return ResponseEntity.ok().body("The car is deleted successfully.");
+        }
+        return ResponseEntity.badRequest().body("Please check the number plate again.");
     }
 
 }
