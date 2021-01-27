59d22dffb134eb04aaeb9c43c80bfee11cfd0994
qixiaoling <x.qi@novi-education.nl>
2021-01-27 14:31:46 +0100

    update customer error solved

--- src/main/java/nl/novi/Backend/repo/InspectionInventoryRepository.java
+++ src/main/java/nl/novi/Backend/repo/InspectionInventoryRepository.java
@@ -7,4 +7,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InspectionInventoryRepository extends JpaRepository<InspectionInventory, InspectionInventoryId> {
+
+    //pseudo code
+    //deleteByNAAMEERSTEIDAndNaamTweedeId(long id 1, long id 2)
+    boolean deleteByIdInspectionIdAndIdInventoryId(Long inspectionId, Long inventoryId);
+
+
+
 }
