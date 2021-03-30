fdb4e1ae38e6b3aca66b1cf7c628f56c1c776b32
qixiaoling <x.qi@novi-education.nl>
2021-03-30 23:16:24 +0200

    added corsConfigurer() modified

--- src/main/java/nl/novi/Backend/controller/CustomerController.java
+++ src/main/java/nl/novi/Backend/controller/CustomerController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin(origins ="http://localhost:3000")
 @RestController
 @RequestMapping("/")
 public class CustomerController {
