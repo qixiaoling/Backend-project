d77383a0b4a4e2fd120d3a70c17c17c1eac1c675
qixiaoling <x.qi@novi-education.nl>
2021-06-14 20:27:58 +0200

    cross-origin

--- src/main/java/nl/novi/Backend/security_config/ApplicationSecurityConfig.java
+++ src/main/java/nl/novi/Backend/security_config/ApplicationSecurityConfig.java
@@ -44,6 +44,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors()
+                .and()
                 .csrf().disable()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -88,6 +90,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
         };
     }
+
 }
 
 
